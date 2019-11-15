551ba330443408a0eab9e75c1e84fa17ce842e61
FuTTiiZ <46918659+FuTTiiZ@users.noreply.github.com>
2019-11-15 13:51:35 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,177 +1,230 @@
-var clicks = 0;
-function calc() { if ($('#re')[0].value.replace(/,/g, '.').split(' ').length > 1) {
-  console.clear();
-  if (clicks < 1) {
-    clicks++;
-  } else {
-    for (var i = 0; i < $('tbody').children().length - 1; i++) {
-      $('tbody')[0].innerHTML = '<tr id="head"> <th>x</th> <th>h(x)</th> <th>f(x)</th> <th>H(x)</th> <th>F(x)</th> <th>x⋅h(x)</th> </tr>';
-      $('div')[3].innerHTML = '</br> <p id="mindst"> Mindsteværdi: </p> <p id="largest"> Størsteværdi: </p> <p id="vari"> Variationsbredden: </p> <p id="midl"> Middeltal: </p> <p id="typet"> Typetal: </p> <p id="kvartilsaet"> Kvartilsæt: </p>';
-    }
-  }
-  var re = $('#re')[0].value.replace(/,/g, '.').split(' '); // Laver et array, re, som står for raw entries.
-  var ec = [...new Set(re)].sort(function(a, b){return a-b}); // Laver et array, ec,  som står for entry categories.
-  if (!$('#dontfill')[0].checked) {
-    var cDecNums = false;
-    var decNums = [];
-    for (var i = 0; i < ec.length; i++) {
-      if (ec[i] % 1 > 0) {
-        cDecNums = true;
-        decNums.push(ec[i] % 1);
-      }
-    }
-    var fec = [];
-    if (cDecNums == true) {
-      decNums = [...new Set(decNums)].sort(function(a, b){return a-b});
-      var incAmount = decNums[0];
+let rObs = [];
+let pObs = [];
+let nObs = [];
 
-      var j = parseFloat(ec[0]);
-      for (var i = 0; i < ((ec[ec.length - 1] - parseFloat(ec[0])) / incAmount); i++) {
-        fec.push((j + (incAmount * i)).toString());
-      }
-      fec.push(ec[ec.length - 1]);
+let totalH = 0;
+let totalH2 = 0;
+let totalF = 0;
+
+let typetal = -999999999999999999999999999999999999999999999999999999999999999999999999999999999999;
+let typetalA = [];
+
+let kvarStat = 1;
+let kvartilSet = [];
+
+let fill = $('#fill')[0].checked;
+let interval = 1;
+
+$('#fill').click(function() {
+  $('#interval').attr('disabled', !$('#fill')[0].checked);
+  fill = $('#fill')[0].checked;
+});
+
+$('#submit').click(function() {
+  if ($('#obs').val().length < 1) return;
+
+  rObs = $('#obs').val().replace(/  /g, ' ').replace(/\./g, ',').split(' ');
+  totalH = 0;
+  totalH2 = 0;
+  totalF = 0;
+  pObs = [];
+  nObs = [];
+  typetal = -999999999999999999999999999999999999999999999999999999999999999999999999999999999999;
+  typetalA = [];
+  kvarStat = 1;
+  kvartilSet = [];
+  fill = $('#fill')[0].checked;
+  interval = parseFloat($('#interval').val().replace('.', ',').replace(',', '.'));
+
+  if (fill && isNaN(interval)) {  
+    alert('Dit interval skal være et tal!');
+    return;
+  }
+
+  let cleared = 0;
+  for (let i = 0; i < rObs.length; i++) {
+    if (!parseFloat(rObs[i].replace(',', '.')) && !parseFloat(rObs[i].replace(',', '.')) === 0) {
+      alert('Du må kun skrive tal!')
+
+      rObs = [];
+      nObs = [];
     } else {
-      for (var i = ec[0]; i < parseFloat(ec[ec.length - 1]) + 1; i++) {
-        fec.push(i.toString());
+      nObs.push(parseFloat(rObs[i].replace(',', '.')));
+      cleared++;
+      if (cleared === rObs.length) {
+        $('table').html(`
+          <tr id="head">
+            <th>x</th>
+            <th>h(x)</th>
+            <th>f(x)</th>
+            <th>H(x)</th>
+            <th>F(x)</th>
+            <th>x⋅h(x)</th>
+          </tr>
+        `);
+        cleared = 0;
+        insertObs();
+      }
+    }
+  }
+});
+
+function isFloat(n) {
+  return Number(n) === n && n % 1 !== 0;
+}
+
+function pretty(num) {
+  let curr = num.toFixed(2).toString().replace('.', ',').replace(',00', '');
+  if (!curr.split(',')[1]) return curr;
+  if (curr.split(',')[1].charAt(1) === '0') {
+    curr = curr.split(',')[0] + ',' + curr.split(',')[1].charAt(0);
+  }
+  return curr;
+}
+
+function h(val, arr, add, type) {
+  let occs = 0;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] === val ? occs++ : 0;
+  }
+  //console.log(totalH2 + ' + ' + occs + ' = ' + (totalH2 + occs));
+  if (add) totalH2 += occs;
+  if (type) {
+    if (occs > typetal) {
+      typetal = occs;
+      typetalA = [];
+      typetalA.push(val);
+    } else if (occs === typetal) {
+      typetalA.push(val);
+    }
+  }
+  return occs;
+}
+
+function f(val, kvartVar) {
+  totalF += (val / totalH * 100);
+  if (totalF >= kvarStat * 25 && kvarStat <= 3) {
+    kvartilSet.push(kvartVar);
+    kvarStat++;
+  }
+  if (totalF >= kvarStat * 25 && kvarStat <= 3) {
+    kvartilSet.push(kvartVar);
+    kvarStat++;
+  }
+  if (totalF >= kvarStat * 25 && kvarStat <= 3) {
+    kvartilSet.push(kvartVar);
+    kvarStat++;
+  }
+
+  return (val / totalH * 100).toFixed(2).toString().replace('.', ',').replace(',00', '');
+}
+
+function insertObs() {
+  nObs = [];
+  for (let i = 0; i < rObs.length; i++) {
+    if (pObs.indexOf(rObs[i]) === -1) {
+      isFloat(rObs[i]) ? pObs.push(pretty(rObs[i])) : pObs.push(rObs[i]);
+      nObs.push(parseFloat(rObs[i].replace(',', '.')));
+    }
+  }
+  if (fill && pObs.length > 1) {
+    let tpObs = [pObs[0]];
+    let tnObs = [nObs[0]];
+
+    let proceed = true;
+    let curr = nObs[0];
+    for (let i = 0; curr < nObs[nObs.length - 1] - interval; i++) {
+      curr += interval;
+      isFloat(curr) ? tpObs.push(pretty(curr)) : tpObs.push(curr.toString());
+      tnObs.push(parseFloat(curr.toString().replace(',', '.')));
+
+      if (!(curr < nObs[nObs.length - 1] - interval) && (curr + interval > nObs[nObs.length - 1])) {
+        alert('Dit interval går ikke op i dine observationer!');
+        proceed = false;
+        tpObs = [pObs[0]];
+        tnObs = [nObs[0]];
       }
     }
 
-    ec = fec;
-  }
+    if (!proceed) return;
 
-  function getOccs(val, arr) {
-    var count = 0;
-    arr.forEach((v) => (v === val && count++));
-    return count;
-  }
+    tpObs.push(pObs[pObs.length - 1]);
+    tnObs.push(nObs[nObs.length - 1]);
 
-  for (var i = 0; i < ec.length; i++) {
-    $('table').append('<tr>' +
-    '<td>' + ec[i].toString().replace('.', ',') + '</td>' +
-    '<td>' + getOccs(ec[i], re) + '</td>' +
-    '<td>' + (getOccs(ec[i], re) / re.length * 100).toString().replace('.', ',') + '%</td>' +
-    '</tr>');
-    console.log(ec[i].toString().replace('.', ',') + ': ' + getOccs(ec[i], re));
-  }
-
-  for (var i = 0; i < $('tbody').children().length - 1; i++) {
-    if (i < 1) {
-      $('tbody').children()[i + 1].innerHTML += ('<td>' +
-      $('tbody').children()[i + 1].children[1].innerText +
-      '</td>');
-    } else {
-      $('tbody').children()[i + 1].innerHTML += ('<td>' +
-      (parseFloat($('tbody').children()[i + 1].children[1].innerHTML) + parseFloat($('tbody').children()[i].children[3].innerHTML)) +
-      '</td>');
-    }
-  }
-
-  for (var i = 0; i < $('tbody').children().length - 1; i++) {
-    if (i < 1) {
-      $('tbody').children()[i + 1].innerHTML += ('<td>' +
-      $('tbody').children()[i + 1].children[2].innerText +
-      '</td>');
-    } else {
-      $('tbody').children()[i + 1].innerHTML += ('<td>' +
-      (parseFloat($('tbody').children()[i + 1].children[2].innerHTML.replace(',', '.')) + parseFloat($('tbody').children()[i].children[4].innerHTML.replace(',', '.'))).toString().replace('.', ',') +
-      '%</td>');
-    }
-  }
-
-  var ft = 0;
-
-  for (var i = 0; i < $('tbody').children().length - 1; i++) {
-    ft += parseFloat($('tbody').children()[i + 1].children[2].innerHTML.replace(',', '.').replace('%', ''))
-  }
-
-  for (var i = 0; i < $('tbody').children().length - 1; i++) {
-    $('tbody').children()[i + 1].innerHTML += ('<td>' +
-    (parseFloat($('tbody').children()[i + 1].children[0].innerHTML) * parseFloat($('tbody').children()[i + 1].children[1].innerHTML)) +
-    '</td>');
-  }
-
-  var xht = 0;
-
-  for (var i = 0; i < $('tbody').children().length - 1; i++) {
-    xht += parseFloat($('tbody').children()[i + 1].children[5].innerHTML.replace(',', '.'));
-  }
-
-  $('table').append('<tr>'+
-  '<td>I alt:</td>'+
-  '<td>' + re.length + '</td>' +
-  '<td>' + ft.toString().replace('.', ',') + '%</td>' +
-  '<td></td>' +
-  '<td></td>' +
-  '<td>' + xht.toString().replace('.', ',') + '</td>' +
-  '</tr>');
-
-  var store = re,
-      distribution = {},
-      max = 0,
-      typet = [];
-
-  store.forEach(function (a) {
-      distribution[a] = (distribution[a] || 0) + 1;
-      if (distribution[a] > max) {
-          max = distribution[a];
-          typet = [a];
-          return;
+    for (v of pObs) {
+      if (tpObs.indexOf(v) === -1) {
+        //alert(v + ' = ' + )
+        alert('Dit interval går ikke op i dine observationer!');
+        proceed = false;
+        tpObs = [pObs[0]];
+        tnObs = [nObs[0]];
       }
-      if (distribution[a] === max) {
-          typet.push(a);
-      }
-  });
+    }
 
-  var kvartilset = '';
-  for (var i = 0; i <$('tbody').children().length - 1; i++) {
-    if (parseFloat($('tbody').children()[i + 1].children[4].innerHTML.replace('%', '').replace(',', '.')) >= 25) {
-      kvartilset += $('tbody').children()[i + 1].children[0].innerHTML;
-      break;
-    }
-  }
-  kvartilset += ', ';
-  for (var i = 0; i <$('tbody').children().length - 1; i++) {
-    if (parseFloat($('tbody').children()[i + 1].children[4].innerHTML.replace('%', '').replace(',', '.')) >= 50) {
-      kvartilset += $('tbody').children()[i + 1].children[0].innerHTML;
-      break;
-    }
-  }
-  kvartilset += ' og ';
-  for (var i = 0; i <$('tbody').children().length - 1; i++) {
-    if (parseFloat($('tbody').children()[i + 1].children[4].innerHTML.replace('%', '').replace(',', '.')) >= 75) {
-      kvartilset += $('tbody').children()[i + 1].children[0].innerHTML;
-      break;
-    }
-  }
-  console.log(kvartilset);
+    pObs = tpObs;
+    nObs = tnObs;
 
-  //Middeltal
-  $('#midl')[0].innerHTML += '<b>' + (xht / re.length).toString().replace('.', ',') + '</b>';
-  //Mindsteværdi
-  $('#mindst')[0].innerHTML += '<b>' + ec[0].toString().replace('.', ',') + '</b>';
-  //Størsteværdi
-  $('#largest')[0].innerHTML += '<b>' + ec[ec.length - 1].toString().replace('.', ',') + '</b>';
-  //Variationsbredden
-  $('#vari')[0].innerHTML += '<b>' + (ec[ec.length - 1] - ec[0]).toString().replace('.', ',') + '</b>';
-  //Typetal
-  if (typet.length > 1) {
-    var typetf = '';
-    for (var i = 0; i < typet.length; i++) {
-      if (i === typet.length - 1) {
-        typetf += typet[i].toString().replace('.', ',');
-      } else if (i === typet.length - 2) {
-        typetf += typet[i].toString().replace('.', ',') + ' og ';
+  }
+
+  for (let i = 0; i < nObs.length; i++) {
+    totalH += h(pObs[i], rObs);
+  }
+
+  nObs.sort(function(a, b){return a-b});
+
+  for (let i = 0; i < pObs.length; i++) {
+    pObs[i] = pObs[i].replace(',00', '');
+  }  
+  
+  let totalXH = 0;
+  for (let i = 0; i < pObs.length; i++) {
+    $('table').html(`${$('table').html()}
+      <tr id="head">
+        <td>${pObs[i]}</td>
+        <td>${h(pObs[i], rObs, true, true)}</td>
+        <td>${f(h(pObs[i], rObs), pObs[i])}%</td>
+        <td>${totalH2.toString().replace('.', ',')}</td>
+        <td>${pretty(totalF)}%</td>
+        <td>${pretty(nObs[i] * h(pObs[i], rObs))}</td>
+      </tr>
+    `);
+    totalXH += (nObs[i] * h(pObs[i], rObs));
+  }
+
+  $('table').html(`${$('table').html()}
+    <tr id="head">
+      <th>I alt:</th>
+      <td>${totalH}</td>
+      <td>${pretty(totalF)}%</td>
+      <td></td>
+      <td></td>
+      <td>${pretty(totalXH)}</td>
+    </tr>
+  `);
+
+  $('#mindst').html(`Mindsteværdi: <b>${pObs[0]}</b>`);
+  $('#largest').html(`Størsteværdi: <b>${pObs[pObs.length - 1]}</b>`);
+  $('#vari').html(`Variationsbredde: <b>${pretty(nObs[nObs.length - 1] - nObs[0])}</b>`);
+  $('#midl').html(`Middeltal: <b>${pretty(totalXH / totalH)}</b>`);
+
+  if (typetalA.length === 1) {
+    $('#typet').html(`Typetal: <b>${typetalA[0]}</b>`);
+  } else if (typetalA.length === 2) {
+    $('#typet').html(`Typetal: <b>${typetalA[0]} og ${typetalA[1]}</b>`)
+  } else if (typetalA.length > 2) {
+    let res = '';
+    for (let i = 0; i < typetalA.length; i++) {
+      if (i === 0) {
+        res += typetalA[i];
+      } else if (i === (typetalA.length - 1)) {
+        res += ' og ' + typetalA[i];
       } else {
-        typetf += typet[i].toString().replace('.', ',') + ', ';
+        res += ', ' + typetalA[i];
       }
     }
-    $('#typet')[0].innerHTML += '<b>' + typetf + '</b>';
-  } else {
-    $('#typet')[0].innerHTML += '<b>' + typet[0].toString().replace('.', ',') + '</b>';
+    $('#typet').html(`Typetal: <b>${res}</b>`);
   }
-  //Kvartilsæt
-  $('#kvartilsaet')[0].innerHTML += '<b>' + kvartilset + '</b>';
 
-} else {alert('Du skal skrive mindst to tal.');}}
+  $('#kvartilsaet').html(`Kvartilsæt: <b>${kvartilSet[0]}, ${kvartilSet[1]} og ${kvartilSet[2]}</b>`)
 
-$('#submit')[0].addEventListener('click', calc);
+}
